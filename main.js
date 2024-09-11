9422f2262bb61397f8577c2e5d1b4a1a13ab53ab
Jesus Macias <jesus.macias.sifuentes@banorte.com>
2024-09-11 09:47:46 -0600

    ...

--- main.js
+++ main.js
@@ -259,7 +259,7 @@ function getQuestions(data){
                 .replaceAll("    ", "\t").replaceAll("\n", "<br>")
                 .replaceAll("\t", "&emsp;&emsp;").replace(/<html/g, '&lt;html').replace(/<tr/g, '&lt;tr')
                 .replace(/<td/g, '&lt;td').replace(/<table/g, '&lt;table ').replace(/<\/t/g, '&lt;\/t').replace(/<\/h/g, '&lt;\/h')
-                .replace(/<\/s/g, '&lt;\/s').replace(/<s/g, '&lt;s');
+                .replace(/<\/s/g, '&lt;\/s').replace(/<s/g, '&lt;s').replace(/<\/di/g, '&lt;\/di').replace(/<di/g, '&lt;di');
                 return text;
             }
             
